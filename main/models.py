fa2de3403832262a370e81b3b21e8af361ad79f5
a-rod85 <31aaronanderson@gmail.com>
2022-09-30 08:48:51 +0000

    add to models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(default='None', max_length=30)
+    Restaurant = models.ForeignKey("Restaurant", on_delete=models.PROTECT)
+    Booking_time = models.DateTimeField(null=False, blank=False, unique=True)
+ 
+
+def __str__(self):
+    return self.Name
+  
+ 
+class Restaurant(models.Model):
+    name = models.CharField(max_length=20*)
+
+
+def __str__(self):
+    return self.name
+
+
+class Table(models.Model):
+    restaurant = models.ForeignKey("Restaurant", on_delete=models.PROTECT)
+    table_number = models.CharField(max_length=2, default="0")
+
+
+def __str__(self):
+    return self.table_number
+
+
+class Seat(models.Model):
+    table = models.ForeignKey("Table", on_delete=models.PROTECT)
+    total_seats = models.IntegerField(default=6)
+    occupied_seats = models.IntegerField(default=0)
